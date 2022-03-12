7bedf4f5252af0c3467c8490a025ea295c1ee2ec
Kevin Chen <kevin@bloxroute.com>
2022-03-11 20:59:41 -0600

    remove default argument

--- backend/fwordle/config/app.py
+++ backend/fwordle/config/app.py
@@ -5,8 +5,6 @@ from fwordle.serializer import Case
 
 @dataclass
 class App:
+    dictionary_path: str
     empty_session_timeout_s: int = 30
     case: Case = Case.CAMEL
-    dictionary_path: str = (
-        "/Users/kevin.chen/workspace/fwordle/backend/data_files/words_alpha.txt"
-    )
